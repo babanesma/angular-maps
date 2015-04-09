89051dbee1da8f7f0c8bd165bf830d252180d9b6
Adham Saad <adham.sa3d@gmail.com>
2015-04-09 15:21:14 +0200

    initial commit - updating bower and preparing the repo

---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+var mapControllers = angular.module('mapControllers', []);
+
+mapControllers.controller('mapMainController', ['$scope', 'Phone',
+    function ($scope, Phone) {
+
+    }]);
