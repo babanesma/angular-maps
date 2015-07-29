06572a4f43b82c160f4f3477d992ef097329181c
Ahmed Samir <ahmed.samir@camelcasetech.com>
2015-07-29 11:15:59 +0300

    last changes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,10 +6,30 @@ var mapsControllers = angular.module('mapsControllers', ['uiGmapgoogle-maps']);
 
 mapsControllers.controller('MapsMainController', ['$scope',
     function ($scope) {
-        $scope.map = { center: { latitude: 30.0500, longitude: 31.2333 }, zoom: 8 };
+        $scope.map = {center: {latitude: 30.0500, longitude: 31.2333}, zoom: 8};
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: 30.0500,
+                longitude: 31.2333
+            },
+            options: {draggable: true},
+            events: {                
+            }
+        };
+        $scope.marker2 = {
+            id: 1,
+            coords: {
+                latitude: 30.0505,
+                longitude: 31.2338
+            },
+            options: {draggable: true},
+            events: {                
+            }
+        };
     }]);
 
-mapsControllers.controller('MapsParamController', ['$scope','$routeParams',
+mapsControllers.controller('MapsParamController', ['$scope', '$routeParams',
     function ($scope, $routeParams) {
-        $scope.map = { center: { latitude: $routeParams.lat, longitude: $routeParams.lang }, zoom: parseInt($routeParams.zoom)};
+        $scope.map = {center: {latitude: $routeParams.lat, longitude: $routeParams.lang}, zoom: parseInt($routeParams.zoom)};
     }]);
